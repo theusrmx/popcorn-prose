0265f40ceeeaee6995389e67db7717b4eeafd614
Matheus Ramos Marcolino <matheus.r.marcolino@gmail.com>
2023-11-22 18:22:51 -0300

    adição de alteração mensagem e navbar qnd logado

--- docs/scripts/home_script.js
+++ docs/scripts/home_script.js
@@ -75,3 +75,78 @@ window.addEventListener('load', () => {
     exibirMensagemAleatoria(mensagensAvaliacaoFilmes);
 });
   
+
+console.log(localStorage.getItem('name'))
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Recupere o nome do usuário do localStorage
+    const nomeUsuario = localStorage.getItem('name');
+
+    // Verifique se o nome do usuário existe e não é nulo
+    if (nomeUsuario) {
+        // Atualize o conteúdo do elemento "Bem vindo, John!"
+        document.querySelector('.welcome-message h1').textContent = `Bem vindo, ${nomeUsuario}!`;
+    }
+
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Recupere o token do localStorage
+    const token = localStorage.getItem('token');
+
+    // Se houver um token, o usuário está logado
+    if (token) {
+        // Remova o link de login
+        const linkLogin = document.querySelector('.navbar-nav .nav-link[href="login.html"]');
+        if (linkLogin) {
+            linkLogin.parentElement.style.display = 'none';
+        }
+
+        // Adicione os links "Meus Filmes" e "Deslogar"
+        const navbarNav = document.querySelector('.navbar-nav');
+        if (navbarNav) {
+            const linkMeusFilmes = criarLink('user.html', 'Meus Filmes');
+            const linkDeslogar = criarLink('#', 'Deslogar', deslogar);
+
+            navbarNav.appendChild(linkMeusFilmes);
+            navbarNav.appendChild(linkDeslogar);
+        }
+    } else {
+        // Se não houver token, o usuário não está logado
+        // Remova os links "Meus Filmes" e "Deslogar"
+        const linkMeusFilmes = document.querySelector('.navbar-nav .nav-link[href="user.html"]');
+        const linkDeslogar = document.querySelector('.navbar-nav .nav-link[href="#"]');
+
+        if (linkMeusFilmes) {
+            linkMeusFilmes.parentElement.style.display = 'none';
+        }
+
+        if (linkDeslogar) {
+            linkDeslogar.parentElement.style.display = 'none';
+        }
+    }
+});
+
+// Função auxiliar para criar um elemento de link
+function criarLink(href, texto, onClick) {
+    const link = document.createElement('li');
+    link.classList.add('nav-item');
+    const a = document.createElement('a');
+    a.classList.add('nav-link');
+    a.href = href;
+    a.textContent = texto;
+    if (onClick) {
+        a.addEventListener('click', onClick);
+    }
+    link.appendChild(a);
+    return link;
+}
+
+// Função de logout
+function deslogar() {
+    // Remova o token do localStorage
+    localStorage.removeItem('token');
+    localStorage.removeItem('name');
+    // Redirecione para a página de login
+    window.location.href = 'login.html';
+}
--- docs/scripts/login_script.js
+++ docs/scripts/login_script.js
@@ -25,6 +25,7 @@ document.getElementById('formLogin').addEventListener('submit', function(event) 
         console.log('Token recebido do servidor:', token);
         // Armazenar o token no localStorage
         localStorage.setItem('token', token);
+        getName(token);
         // Redirecionar para a página principal
         window.location.href = 'index.html';
     })
@@ -33,4 +34,32 @@ document.getElementById('formLogin').addEventListener('submit', function(event) 
         alert("Erro ao entrar.");
     });
 });
+
+
+// Função para obter o nome do usuário do servidor
+async function getName(token) {
+    try {
+        // Verificar se o token está presente
+        if (!token) {
+            console.error('O usuário não está autenticado.');
+            return;
+        }
+
+        const response = await fetch('http://localhost:8080/auth/get-name', {
+            method: 'GET'
+        });
+
+        if (!response.ok) {
+            console.error('Erro ao obter o nome do usuário');
+            return;
+        }
+
+        const name = await response.text();
+        console.log('Nome do usuário:', name);
+        // Armazenar o nome no localStorage
+        localStorage.setItem('name', name);
+    } catch (error) {
+        console.error('Erro:', error.message);
+    }
+}
 

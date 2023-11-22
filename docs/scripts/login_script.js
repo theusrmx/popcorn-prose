53b0028beb13a460f921f2d92a5c90f4e7ffc480
Matheus Ramos Marcolino <matheus.r.marcolino@gmail.com>
2023-11-22 17:31:13 -0300

    Adição de cadastro + login

--- docs/scripts/login_script.js
+++ docs/scripts/login_script.js
@@ -1,10 +1,36 @@
-import { iniciarSlides } from "./image_slider.js";
+document.getElementById('formLogin').addEventListener('submit', function(event) {
+    event.preventDefault();
 
-const API_KEY = '557439512040e55c35f758f339c8e1d1';
-const BASE_URL = 'https://image.tmdb.org/t/p/original/';
-const BASE_URL_LOGO = 'https://image.tmdb.org/t/p/original/';
-let interval = 5000;
+    var formData = new FormData(event.target);
 
+    const loginData = {
+        email: formData.get('login'),
+        senha: formData.get('senha')
+    }
 
-const slideShowLogin = iniciarSlides(API_KEY, BASE_URL, BASE_URL_LOGO, interval, 'wallpaper', 'fade');
-slideShowLogin();
+    fetch('http://localhost:8080/auth/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(loginData)
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Erro ao entrar. Por favor, tente novamente.');
+        }
+        return response.text();
+    })
+    .then(token => {
+        console.log('Token recebido do servidor:', token);
+        // Armazenar o token no localStorage
+        localStorage.setItem('token', token);
+        // Redirecionar para a página principal
+        window.location.href = 'index.html';
+    })
+    .catch(error => {
+        console.error('Erro ao entrar:', error.message);
+        alert("Erro ao entrar.");
+    });
+});
+
